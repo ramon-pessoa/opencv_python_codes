e06854efbcfd1b0b183b7f05a2d9d08704c9bfde
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2018-05-18 12:45:39 -0400

    Prog. 1: open, show and save an image

---
+++ test1_open_show_save_image.py
@@ -0,0 +1,23 @@
+# Opening an image, showing an image, saving an image
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('diving.jpg', cv2.IMREAD_GRAYSCALE) # cv2.IMREAD_GRAYSCALE = 0
+# img = cv2.imread('diving.jpg', cv2.IMREAD_COLOR) # cv2.IMREAD_GRAYSCALE = 1
+# img = cv2.imread('diving.jpg', cv2.IMREAD_UNCHANGED) # cv2.IMREAD_GRAYSCALE = -1
+
+# Plotting with OpenCV
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# Plotting with Matplotlib
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+# Drawing a line
+# plt.plot([50, 100], [80, 100], 'c', linewidth=5)
+plt.show()
+
+# To save an image
+cv2.imwrite('diving_gray.png', img)
