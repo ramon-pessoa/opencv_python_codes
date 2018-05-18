98e7b47ed82cfec72ae0ae628c9697430aa8d6f6
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2018-05-18 15:08:20 -0400

    test2_video_capture

--- test1_open_show_save_image.py
+++ test1_open_show_save_image.py
@@ -4,9 +4,98 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('diving.jpg', cv2.IMREAD_GRAYSCALE) # cv2.IMREAD_GRAYSCALE = 0
+img = cv2.imread('frame_130_imgcropped.png', cv2.IMREAD_GRAYSCALE) # cv2.IMREAD_GRAYSCALE = 0
 # img = cv2.imread('diving.jpg', cv2.IMREAD_COLOR) # cv2.IMREAD_GRAYSCALE = 1
 # img = cv2.imread('diving.jpg', cv2.IMREAD_UNCHANGED) # cv2.IMREAD_GRAYSCALE = -1
+
+# Total number of pixels
+img = cv2.imread('frame_130_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_130_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_170_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_170_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_200_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_200_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_230_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_230_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_260_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_260_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_290_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_290_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_320_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_320_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_350_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_350_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_380_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_380_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_410_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_410_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_440_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_440_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_470_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_470_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_500_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_500_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_530_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_530_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_560_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_560_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_570_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_570_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_580_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_580_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_590_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_590_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_600_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_600_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_610_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_610_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_620_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_620_imgcropped.png. Total number of pixels = ", nPixels)
+
+img = cv2.imread('frame_630_imgcropped.png', cv2.IMREAD_GRAYSCALE)
+nPixels = img.size;
+print("frame_630_imgcropped.png. Total number of pixels = ", nPixels)
 
 # Plotting with OpenCV
 # cv2.imshow('image', img)
---
+++ test2_video_capture.py
@@ -0,0 +1,31 @@
+# 
+
+import cv2
+import numpy as np
+
+#cap = cv2.VideoCapture(0) # computer camera 0
+#cap = cv2.VideoCapture(1) # computer camera 1, and so on
+# cap = cv2.VideoCapture('diving.mp4')
+cap = cv2.VideoCapture('diving2.avi')
+
+count = 0
+while True:
+	ret, frame = cap.read() # ret = True or False, frame = video frame
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	cv2.imshow('frame', frame)
+	cv2.imshow('gray', gray)
+
+	if (count % 10 == 0):
+		# To save an image
+		imgname = "frame_" + str(count) + ".png"
+		cv2.imwrite("frames2/"+imgname, frame)
+	
+	count = count + 1
+
+	# To get out of the loop, press any keyboard key
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
