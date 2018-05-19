eec0b98d4aa271b3d8a495b004ed25e4b895eda4
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2018-05-18 22:17:32 -0400

    Updating test2_video_capture.py

--- test2_video_capture.py
+++ test2_video_capture.py
@@ -7,24 +7,29 @@ import numpy as np
 #cap = cv2.VideoCapture(1) # computer camera 1, and so on
 cap = cv2.VideoCapture('diving.mp4')
 
-count = 0
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+# count = 0
 while True:
 	ret, frame = cap.read() # ret = True or False, frame = video frame
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+	# out.write(frame)
 	cv2.imshow('frame', frame)
 	cv2.imshow('gray', gray)
 
-	if (count % 10 == 0):
-		# To save an image
-		imgname = "frame_" + str(count) + ".png"
-		cv2.imwrite("frames2/"+imgname, frame)
+	# if (count % 10 == 0):
+	# 	# To save an image
+	# 	imgname = "frame_" + str(count) + ".png"
+	# 	cv2.imwrite("frames/"+imgname, frame)
 	
-	count = count + 1
+	# count = count + 1
 
 	# To get out of the loop, press any keyboard key
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
