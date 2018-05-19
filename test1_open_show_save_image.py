330f3fcb9d61b5cb88aca074eea8b1c1e3afb9fc
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2018-05-18 21:32:14 -0400

    Updating test1_open_show_save_image.py

--- test1_open_show_save_image.py
+++ test1_open_show_save_image.py
@@ -2,7 +2,7 @@
 
 import cv2
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 img = cv2.imread('diving.jpg', cv2.IMREAD_GRAYSCALE) # cv2.IMREAD_GRAYSCALE = 0
 # img = cv2.imread('diving.jpg', cv2.IMREAD_COLOR) # cv2.IMREAD_GRAYSCALE = 1
@@ -12,15 +12,15 @@ nPixels = img.size;
 print("diving.jpg . Total number of pixels = ", nPixels)
 
 # Plotting with OpenCV
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 # Plotting with Matplotlib
-# plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.imshow(img, cmap='gray', interpolation='bicubic')
 # Drawing a line
 # plt.plot([50, 100], [80, 100], 'c', linewidth=5)
-# plt.show()
+plt.show()
 
 # To save an image
 cv2.imwrite('diving_gray.png', img)
