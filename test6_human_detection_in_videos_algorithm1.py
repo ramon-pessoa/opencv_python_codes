2ef18b2d26d23ecdc349d765e88be17c751e1973
ramonpessoa <ramon@ordigraphe.com>
2018-07-04 11:30:37 -0400

    Code refactoring

--- test6_human_detection_in_videos_algorithm1.py
+++ test6_human_detection_in_videos_algorithm1.py
@@ -9,26 +9,52 @@ import argparse
 import imutils
 import cv2
 
+#####################################################################
+# Settings
+save_video = False
+video_width = 400
+video_height = 225
+fps = 20.0 # fps = frames per second
+#FourCC code is passed as cv2.VideoWriter_fourcc('M','J','P','G') or cv2.VideoWriter_fourcc(*'MJPG') for MJPG.
+fourcc_format = 'XVID' # 'XVID', 'MJPG', 'MP4V'
+show_frame_size = True
+use_original_frame_size = False
+use_web_cam = False
+input_video_path_and_name = 'diving.mp4'
+output_video_path_and_name = 'output.avi'
+#####################################################################
+
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-# cap = cv2.VideoCapture(0) # computer camera 0
-#cap = cv2.VideoCapture(1) # computer camera 1, and so on
-cap = cv2.VideoCapture('diving.mp4')
+if use_web_cam:
+	cap = cv2.VideoCapture(0) # computer camera 0
+	#cap = cv2.VideoCapture(1) # computer camera 1, and so on
+else:
+	cap = cv2.VideoCapture(input_video_path_and_name)
 
 # Define the codec and create VideoWriter object
-#fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+if save_video:
+	fourcc = cv2.VideoWriter_fourcc(*FourCC_format)
+	out = cv2.VideoWriter(output_video_path_and_name,fourcc, fps, (video_width, video_height))
 
 # count = 0
 while True:
 	ret, frame = cap.read() # ret = True or False, frame = video frame
 
-	# Original image
-	image = frame.copy()
-	# Image resized
-	#image = imutils.resize(frame, width=min(400, frame.shape[1]))
+	if use_original_frame_size:
+		# Original image
+		image = frame.copy()
+	else:
+		# Image resized
+		image = imutils.resize(frame, width=min(400, frame.shape[1]))
+	
+	# Print image height, image width, and image channels
+	if show_frame_size:
+		height, width, channels = image.shape
+		print("height, width, channels: ", height, width, channels)
+		show_frame_size = False
 	
 	orig = image.copy()
 
@@ -54,12 +80,14 @@ while True:
 	cv2.imshow("Human Detection Before Non-Maxima Suppression", orig)
 	cv2.imshow("Human Detection After Non-Maxima Suppression", image)
 	
-	#out.write(image)
+	if save_video:
+		out.write(image)
 
 	# To get out of the loop, press any keyboard key
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
 cap.release()
-#out.release()
+if save_video:
+	out.release()
 cv2.destroyAllWindows()
