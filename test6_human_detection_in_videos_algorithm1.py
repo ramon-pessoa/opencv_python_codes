49629625f8efaecc0796f3c2b7ba7ae843f3d8f3
ramonpessoa <ramon@ordigraphe.com>
2018-07-04 11:41:55 -0400

    Code refactoring

--- test6_human_detection_in_videos_algorithm1.py
+++ test6_human_detection_in_videos_algorithm1.py
@@ -19,7 +19,7 @@ fps = 20.0 # fps = frames per second
 #FourCC code is passed as cv2.VideoWriter_fourcc('M','J','P','G') or cv2.VideoWriter_fourcc(*'MJPG') for MJPG.
 fourcc_format = 'XVID' # 'XVID', 'MJPG', 'MP4V'
 show_frame_size = True
-use_original_frame_size = True
+use_original_frame_size = False
 use_web_cam = False
 input_video_path_and_name = 'diving.mp4'
 output_video_path_and_name = 'output.avi'
